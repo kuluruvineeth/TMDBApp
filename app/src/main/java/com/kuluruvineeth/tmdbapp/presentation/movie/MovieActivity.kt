6b86a92c5fdb4392c40329906ac348c044419eb9
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-08-03 06:57:46 +0530

    added update menu options and functionality for movies.

--- app/src/main/java/com/kuluruvineeth/tmdbapp/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/kuluruvineeth/tmdbapp/presentation/movie/MovieActivity.kt
@@ -3,6 +3,9 @@ package com.kuluruvineeth.tmdbapp.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -48,6 +51,38 @@ class MovieActivity : AppCompatActivity() {
             }else{
                 binding.movieProgressBar.visibility = View.GONE
                 Toast.makeText(applicationContext,"No data available",Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflator : MenuInflater = menuInflater
+        inflator.inflate(R.menu.update,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    private fun updateMovies(){
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val response = movieViewModel.updateMovies()
+        response.observe(this, Observer {
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            }else{
+                binding.movieProgressBar.visibility = View.GONE
             }
         })
     }
