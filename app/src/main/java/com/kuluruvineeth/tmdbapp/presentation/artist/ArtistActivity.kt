9719167b5f324006f6e9c3d70d01ce0be65341b3
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-08-03 07:41:24 +0530

    Created recyclerview and menu options for artists.

--- app/src/main/java/com/kuluruvineeth/tmdbapp/presentation/artist/ArtistActivity.kt
+++ app/src/main/java/com/kuluruvineeth/tmdbapp/presentation/artist/ArtistActivity.kt
@@ -2,14 +2,86 @@ package com.kuluruvineeth.tmdbapp.presentation.artist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kuluruvineeth.tmdbapp.R
 import com.kuluruvineeth.tmdbapp.databinding.ActivityArtistBinding
+import com.kuluruvineeth.tmdbapp.presentation.di.Injector
+import javax.inject.Inject
 
 class ArtistActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: ArtistViewModelFactory
+    private lateinit var artistViewModel: ArtistViewModel
     private lateinit var binding: ActivityArtistBinding
+    private lateinit var adapter: ArtistAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_artist)
+        (application as Injector).createArtistSubComponent()
+            .inject(this)
+
+        artistViewModel = ViewModelProvider(this,factory)[ArtistViewModel::class.java]
+        initRecyclerView()
+    }
+    private fun initRecyclerView(){
+        binding.artistRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = ArtistAdapter()
+        binding.artistRecyclerView.adapter = adapter
+        displayPopularArtists()
+    }
+
+    private fun displayPopularArtists(){
+        binding.artistProgressBar.visibility = View.VISIBLE
+        val responseLiveData = artistViewModel.getArtists()
+        responseLiveData.observe(this, Observer {
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.artistProgressBar.visibility = View.GONE
+            }else{
+                binding.artistProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext,"No data available", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflator : MenuInflater = menuInflater
+        inflator.inflate(R.menu.update,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_update -> {
+                updateArtists()
+                true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    private fun updateArtists(){
+        binding.artistProgressBar.visibility = View.VISIBLE
+        val response = artistViewModel.updateArtists()
+        response.observe(this, Observer {
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.artistProgressBar.visibility = View.GONE
+            }else{
+                binding.artistProgressBar.visibility = View.GONE
+            }
+        })
     }
 }
